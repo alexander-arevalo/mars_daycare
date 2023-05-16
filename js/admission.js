92c5f59cd275ddd75a247057c89cc306cd8b3930
zerootwooooo <zanderarevalo20@gmail.com>
2023-05-16 16:14:53 +0800

    quick update

--- js/admission.js
+++ js/admission.js
@@ -1,28 +1,30 @@
 //  to freeze na navbar 
+const axios = require('axios');
 
 window.addEventListener('scroll', () => {
     document.querySelector('nav').classList.toggle
     ('window-scroll', window.scrollY > 0)
 })
 
-function uploadinfo (){
-    axios
-}
 
+// Define your function
+const myForm = document.getElementById('myForm');
 
-//reservation value
+// Define your submit event handler function
+myForm.addEventListener('submit', function(event) {
+    event.preventDefault();
+  
+    // Access form input values using their names
+    const firstName = myForm.elements['First Name'].value;
+    const lastName = myForm.elements['Last Name'].value;
+    const email = myForm.elements['Email Address'].value;
+    const contact = myForm.elements['Mobile Number'].value;
+    const message = myForm.elements['Message'].value;
+  
+    // Perform desired actions with the form data
+    console.log('Form submitted!');
+    console.log(message + ' ' + lastName);
+  });
 
-function reservation () {
-
-    var firstname = document.getElementById("firstname").value;
-    
-    var lastname = document.getElementById("lastname").value;
-
-    var email = document.getElementById("email").value;
-
-    var contact = document.getElementById("contact").value;
-
-    var message = document.getElementById("message").value;
-
-
-}
+// Attach the submit event listener to the form
+myForm.addEventListener('submit', handleSubmit);
