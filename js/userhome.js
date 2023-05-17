70b33135d5174c974e87c9711b422ed68bae139c
zerootwooooo <zanderarevalo20@gmail.com>
2023-05-17 21:44:03 +0800

    profile update

--- js/userhome.js
+++ js/userhome.js
@@ -1,5 +1,16 @@
-const Signout = document.getElementById("signout")
+const signoutLink = document.getElementById("sub__menu__link");
 
-Signout.addEventListener("click",()=> {
-    window.location.replace("index.html")
-})
+signoutLink.addEventListener("click", () => {
+  window.location.replace("index.html");
+});
+
+let subMenu = document.getElementById('subMenu');
+
+function openProfile() {
+  subMenu.classList.toggle('open__menu');
+ 
+}
+
+image.addEventListener('click',openProfile);
+var image = document.getElementById("myImage"); 
+
