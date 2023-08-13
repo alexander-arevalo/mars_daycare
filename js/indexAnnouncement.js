20d3d08bb453d71fd90be7d7adaa1c95e3d9a0d9
zerootwooooo <zanderarevalo20@gmail.com>
2023-08-14 06:25:45 +0800

    Update indexAnnouncement.js

--- js/indexAnnouncement.js
+++ js/indexAnnouncement.js
@@ -15,29 +15,3 @@ const getAnnouncement = async () => {
   announcement.textContent = data.data.resp[0].announcementDescription;
 };
 getAnnouncement();
-
-// JavaScript to handle the dropdown functionality
-const dropdownToggle = document.querySelector(".dropdown > a");
-const dropdownMenu = document.querySelector(".dropdown-menu");
-
-let dropdownOpen = false;
-dropdownToggle.addEventListener("click", (event) => {
-  event.preventDefault();
-  if (dropdownOpen) {
-    dropdownMenu.classList.remove("show");
-  } else {
-    dropdownMenu.classList.add("show");
-  }
-  dropdownOpen = !dropdownOpen;
-});
-
-// Close the dropdown if the user clicks outside of it
-window.addEventListener("click", (event) => {
-  if (
-    !dropdownToggle.contains(event.target) &&
-    !dropdownMenu.contains(event.target)
-  ) {
-    dropdownMenu.classList.remove("show");
-    dropdownOpen = false;
-  }
-});
